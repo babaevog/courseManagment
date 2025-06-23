86213d5bba5e3b0771106ceac1620e07b433d667
babaevog <aikoxworld@gmail.com>
2025-06-23 21:00:01 +0300

    Добавлена регистрация и модель User

--- CourseManagment/Migrations/20250623173712_AdUserTable.Designer.cs
+++ CourseManagment/Migrations/20250623173712_AdUserTable.Designer.cs
@@ -4,6 +4,7 @@ using CourseManagment.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CourseManagment.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250623173712_AdUserTable")]
+    partial class AdUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
